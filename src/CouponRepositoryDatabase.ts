c07adf902519ecdecf574b44cb8746de42d9f6a9
LinaAmaral <lina.amaralcaetano@gmail.com>
2023-05-28 11:40:12 -0300

    ajuste da string de conexão

--- src/CouponRepositoryDatabase.ts
+++ src/CouponRepositoryDatabase.ts
@@ -4,7 +4,7 @@ import CouponRepository from "./CouponRepository";
 export default class CouponRepositoryDatabase implements CouponRepository {
 
     async get(code: string) {
-        const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+        const connection = pgp()("postgres://postgres:12345@localhost:5433/app");
         const [couponData] = await connection.query("select * from cccat11.coupon where code = $1", [code]);
         await connection.$pool.end();
         return couponData;
--- src/ProductRepositoryDataBase.ts
+++ src/ProductRepositoryDataBase.ts
@@ -4,7 +4,7 @@ import ProductRepository from "./ProductRepository";
 export default class ProductRepositoryDatabase implements ProductRepository {
 
     async get(idProduct: number) {
-        const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+        const connection = pgp()("postgres://postgres:12345@localhost:5433/app");
         const [productData] = await connection.query("select * from cccat11.product where id_product = $1", [idProduct]);
         await connection.$pool.end();
         return productData;
