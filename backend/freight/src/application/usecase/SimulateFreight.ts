21ed7df317d5887ee257aab3e9faccddef68d8c9
LinaAmaral <lina.amaralcaetano@gmail.com>
2024-01-11 10:48:36 -0300

    comentário da lógica

--- backend/freight/src/application/usecase/SimulateFreight.ts
+++ backend/freight/src/application/usecase/SimulateFreight.ts
@@ -16,15 +16,11 @@ export default class SimulateFreight {
         };
         for (const item of input.items) {
             if (input.from && input.to) {
-                const from = await this.zipCodeRepository.get(input.from)
+                const from = await this.zipCodeRepository.get(input.from)//dado um cep eu busco as coordenadas
                 const to = await this.zipCodeRepository.get(input.to)
-                let distance = 1000;
-                if (from && to) {
-                    distance = DistanceCalculator.calculate(from.coord, to.coord)
-                }
-
-
-                const freight = FreightCalculator.calculate(distance, item.volume, item.density);
+                let distance = 1000; //por default a distancia é 1000
+                if (from && to) distance = DistanceCalculator.calculate(from.coord, to.coord)//dado as coordenadas entre dois pontos eu calculo a distancia
+                const freight = FreightCalculator.calculate(distance, item.volume, item.density);//dado a distancia e as dimenções do produto eu calculo o frete
                 output.freight += freight * item.quantity;
             }
         }
